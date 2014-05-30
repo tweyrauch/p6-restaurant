db7e0b82820578d6b30585a76fafcc1d7aa05aa7
tweyrauch <tiayrock@gmail.com>
2014-05-30 15:38:50 -0700

    Version 15
    *tried to make accordion on small screen

--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,19 @@ $(document).ready(function(){
 $('#fullpage').fullpage({
 	anchors: ['home', 'about', 'food', 'contact'],
 	scrollingSpeed: 1000,
-	slidesColor: ['black', '#D9261C', 'orange', 'yellow'],
+	slidesColor: ['black', '#D9261C', '#FFAC07', '#A5BD60'],
 	scrollOverflow:true,
 	paddingTop: '3em',
 	menu:'.sub-nav'
 
 });
+});
 
 
-
-
-
-
+$(window).resize(function(){
+if($(window).width()<=320){
+	$('.scroller').remove();
+	$('dl').removeAttr('id').addClass('accordion');
+	$('dd').removeClass('section');
+}
 });
